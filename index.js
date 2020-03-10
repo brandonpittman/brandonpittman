b6e0db71d7f970100e4e6ec58dc5b78a859eaec6
Brandon Pittman <brandon@brandonpittman.net>
2020-03-10 17:35:49 +0900

    ⚡️ Use ncc to bundle deps

--- index.js
+++ index.js
@@ -8,33 +8,29 @@ const path = require('path')
 
 // Define options for Boxen
 const options = {
-  padding: 1,
-  margin: 1,
+  padding: 2,
+  margin: 2,
   borderStyle: 'round'
 }
 
 // Text + chalk definitions
 const data = {
   name: chalk.white('              Brandon Pittman'),
-  handle: chalk.white('brandonpittman'),
-  work: chalk.white('Web Director @ Aquaring Global Strategy'),
-  opensource:
-    chalk.white('OmniFocus | Gridsome | Tailwind | PurgeCSS ') +
-    chalk.green('⬢'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('brandonpittman'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~brandonpittman'),
-  github: chalk.gray('https://github.com/') + chalk.green('brandonpittman'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  wantedly:
-    chalk.gray('https://www.wantedly.com/users/') + chalk.blue('80873258'),
-  web: chalk.cyan('https://brandonpittman.net'),
-  npx: chalk.red('npx') + ' ' + chalk.white('brandonpittman'),
+  handle: chalk`{white brandonpittman}`,
+  work: chalk`{white Web Director @ Aquaring Global Strategy}`,
+  //work: chalk.white('Frontend Wizard @ Quartet Communications'),
+  opensource: chalk`{white.bold Vue | Gridsome | Node.js | Tailwind} {green.bold ⬢}`,
+  twitter: chalk`{gray https://twitter.com/}{cyan brandonpittman}`,
+  npm: chalk`{gray https://npmjs.com/}{red ~brandonpittman}`,
+  github: chalk`{gray https://github.com/}{green brandonpittman}`,
+  wantedly: chalk`{gray https://www.wantedly.com/users/}{blue 80873258}`,
+  web: chalk`{cyan https://brandonpittman.net}`,
+  npx: chalk`{red npx} {white brandonpittman}`,
   labelWork: chalk.white.bold('       Work:'),
   labelOpenSource: chalk.white.bold('Open Source:'),
   labelTwitter: chalk.white.bold('    Twitter:'),
   labelnpm: chalk.white.bold('        npm:'),
   labelGitHub: chalk.white.bold('     GitHub:'),
-  //  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
   labelWantedly: chalk.white.bold('   Wantedly:'),
   labelWeb: chalk.white.bold('        Web:'),
   labelCard: chalk.white.bold('       Card:')
@@ -48,7 +44,6 @@ const opensourcing = `${data.labelOpenSource}  ${data.opensource}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const wantedlying = `${data.labelWantedly}  ${data.wantedly}`
 const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
@@ -77,7 +72,10 @@ const output =
   newline +
   carding
 
-fs.writeFileSync(
-  path.join(__dirname, 'bin/output'),
-  chalk.green(boxen(output, options))
-)
+// fs.writeFileSync(
+//   path.join(__dirname, 'bin/output'),
+//   chalk.blue(boxen(output, options))
+// )
+
+const boxedText = chalk.blue(boxen(output, options))
+console.log(boxedText)
